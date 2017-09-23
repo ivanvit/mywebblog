a387c64805b1cd49f77ed4c923a4be2a285de8b4
Vitaliy Ivaneev <ivanvit@mail.ru>
2017-09-23 11:37:02 +0300

    45

--- blog/superblog/urls.py
+++ blog/superblog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [url(r'^$', views.IndexView, name='index'),
                url(r'^(?P<pk>[0-9]+)/$', views.DetailView, name='detail'),
-               url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView, name='results'),
+               url(r'^(?P<pkgit checkou git rm >[0-9]+)/results/$', views.ResultsView, name='results'),
                url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
                ]
+f=0
